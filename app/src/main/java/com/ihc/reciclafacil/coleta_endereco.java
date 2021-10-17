6c2c19e96aea5d56b2aeaa68bc1eee66811997fc
Tiago Magnus <tiago.magnus@avidity.se>
2021-10-17 16:39:18 -0300

    Telas de agendamento

---
+++ app/src/main/java/com/ihc/reciclafacil/coleta_endereco.java
@@ -0,0 +1,14 @@
+package com.ihc.reciclafacil;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class coleta_endereco extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_coleta_endereco);
+    }
+}
---
+++ app/src/main/java/com/ihc/reciclafacil/coleta_tipo.java
@@ -0,0 +1,14 @@
+package com.ihc.reciclafacil;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class coleta_tipo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_coleta_tipo);
+    }
+}
