efc326214b8d1868b3e1b1631533c1d76396d02c
PauloGamarra <paulogamarralessa@gmail.com>
2021-10-18 22:22:58 -0300

    telas de menu incial, resgate de pontos e historico de descartes

---
+++ app/src/main/java/com/ihc/reciclafacil/historico_descartes.java
@@ -0,0 +1,14 @@
+package com.ihc.reciclafacil;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class historico_descartes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_historico_descartes);
+    }
+}
